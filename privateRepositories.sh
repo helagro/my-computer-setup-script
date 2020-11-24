ba4e19dca6680a8623a20d2ab35ae998d6671741
Henrik Lagrosen <henrikl@tuta.io>
2020-11-24 18:20:30 +0100

    adds pause checklist

--- privateRepositories.sh
+++ privateRepositories.sh
@@ -3,5 +3,6 @@
 cd ~/github
 
 git clone --recurse-submodules https://github.com/helagro/day-eval-checklist
+git clone --recurse-submodules https://github.com/helagro/pause-checklist
 
 scripts/installAllRepos.sh
